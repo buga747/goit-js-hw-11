7b817aea6a2e7cc46fd99d41267c0482d02d78ac
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-10 23:31:52 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+fetch(
+  'https://pixabay.com/api/?key=32739327-960c57fb59b81838da1bafdd1&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1'
+)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch('error');
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
