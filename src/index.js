c6f57489c3e77763040356bfc1f3dc65fdb75885
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-12 18:22:53 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
-fetch(
-  'https://pixabay.com/api/?key=32739327-960c57fb59b81838da1bafdd1&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1'
-)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch('error');
-
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
-});
+import { fetchPixabayAPI } from './js/pixabay-api.js';
+import { renderGallery } from './js/render-gallery';
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '32739327-960c57fb59b81838da1bafdd1';
+
+async function fetchPixabayAPI(query, page = 1, perPage = 40) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  console.log(response);
+  return response;
+}
+
+export { fetchPixabayAPI };
