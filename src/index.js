60a394a263d3807336edf8c3d7e11bf563e68820
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-01-13 23:29:19 +0200

    js-end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,102 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchPixabayAPI } from './js/pixabay-api.js';
 import { renderGallery } from './js/render-gallery';
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+let simpleLightBox;
+
+let query = '';
+let page = 1;
+let perPage = 40;
+
+form.addEventListener('submit', onSearch);
+
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(evt) {
+  evt.preventDefault();
+
+  gallery.innerHTML = '';
+  loadMoreBtn.hidden = true;
+  page = 1;
+  //   const {
+  //     searchQuery: { value: query },
+  //   } = evt.currentTarget.elements;
+  query = evt.currentTarget.searchQuery.value.trim();
+  console.log(query);
+
+  if (query === '') {
+    insertInfo();
+    return;
+  }
+
+  fetchPixabayAPI(query, page, perPage)
+    .then(({ data }) => {
+      if (data.hits.length === 0) {
+        onNoResult();
+      } else {
+        renderGallery(data.hits);
+
+        onSimplelightboxAdd();
+        onImagesFound(data);
+
+        if (data.totalHits > perPage) {
+          loadMoreBtn.hidden = false;
+        }
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(form.reset());
+}
+
+function onLoadMore() {
+  page += 1;
+  simpleLightBox.destroy();
+
+  fetchPixabayAPI(query, page, perPage)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      onSimplelightboxAdd();
+
+      const totalPages = Math.ceil(data.totalHits / perPage);
+
+      if (page === totalPages) {
+        loadMoreBtn.hidden = true;
+        onCollectionEnd();
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function insertInfo() {
+  Notify.warning('You need to insert information to find');
+}
+
+function onNoResult() {
+  Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function onImagesFound(data) {
+  Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function onCollectionEnd() {
+  Notify.warning("We're sorry, but you've reached the end of search results.");
+}
+
+function onSimplelightboxAdd() {
+  simpleLightBox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionSelector: 'img',
+    captionPosition: 'bottom',
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionDelay: 250,
+  }).refresh();
+}
--- src/js/render-gallery.js
+++ src/js/render-gallery.js
@@ -14,28 +14,31 @@ function renderGallery(images) {
         comments,
         downloads,
       }) => {
-        return;
-        `<a class="gallery__link" href="${largeImageURL}">
+        return `<a class="gallery__link" href="${largeImageURL}">
       <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      
+        <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        
         <div class="info">
           <p class="info-item">
-            <b>Likes</b> ${likes}
+            <b>Likes: </b> ${likes}
           </p>
           <p class="info-item">
-            <b>Views</b>${views}
+            <b>Views: </b>${views}
           </p>
           <p class="info-item">
-            <b>Comments</b>${comments}
+            <b>Comments: </b>${comments}
           </p>
           <p class="info-item">
-            <b>Downloads</b>${downloads}
+            <b>Downloads: </b>${downloads}
           </p>
         </div>
       </div>
-      </a>`;
+      </a>
+      `;
       }
     )
     .join('');
+
   gallery.insertAdjacentHTML('beforeend', markup);
 }
